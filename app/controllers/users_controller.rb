9e5de96427a229b26f84b682c97f72725c17ab4b
Steven Wing <steven.wing82@gmail.com>
2019-07-21 10:13:52 -0700

    connect to frontend with DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
     def index
+
         render :json => {
-            message: 'it works...?'
+            message: 'Confidential'
         }
     end
+
+    def show
+        if User.exists?(params[:id])
+            userId = User.find(params[:id])
+            agentName = userId.surname
+            message = "Welcome back Agent #{agentName}"
+        else
+            message = "Unauthorized access!"
+        end
+
+        render :json => {
+            message: message
+        }
+            
+    end
+
 
 end
