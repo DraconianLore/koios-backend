9f702aaa893ee8ede4bd94785b55ef52c30564d1
Steven Wing <steven.wing82@gmail.com>
2019-07-21 17:04:27 -0700

    db modifications

--- db/migrate/20190721232256_change_mission_types_to_mission_types.rb
+++
@@ -1,5 +0,0 @@
-class ChangeMissionTypesToMissionTypes < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :mission_types, :missionTypes
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+user = User.new
+user.surname = 'Smith'
+user.firstName = 'Mister'
+user.email = 'smith@agents.matrix'
+user.password = 'smith'
+user.rank = 2
+user.experience = 20
+user.save!
+
+# Missions
+miss = Mission.new
+miss.status = 'complete'
+miss.experience = 10
+miss.startTime = 1.day.ago
+miss.endTime = 18.hours.ago
+miss.difficulty = 'Easy'
+miss.user = User.first
+miss.save!
+
+miss = Mission.new
+miss.status = 'failed'
+miss.experience = 10
+miss.startTime = 12.hours.ago
+miss.endTime = 11.hours.ago
+miss.difficulty = 'Easy'
+miss.user = User.first
+miss.save!
+
+miss = Mission.new
+miss.status = 'open'
+miss.experience = 12
+miss.difficulty = 'Easy'
+miss.user = User.first
+miss.save!
+
+puts '##### Seeding complete #####'
